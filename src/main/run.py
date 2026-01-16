b7668c6221069829f8e0b9ca6dc7375abc7a84f1
Daniel <danil.malaev3@gmail.com>
2026-01-16 12:20:52 +0200

    change proj. struct

---
+++ src/main/run.py
@@ -0,0 +1,12 @@
+from settings import setting
+import asyncio
+from aiogram import Bot, Dispatcher
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+
+bot = Bot(token=TG_BOT_API_KEY)
+
+dispatcher = Dispatcher()
+
+
+
--- src/settings/logging_config.py
+++
@@ -1,4 +0,0 @@
-import logging
-from settings import config
-
-
