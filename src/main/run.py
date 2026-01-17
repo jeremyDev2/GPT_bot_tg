65015644a68cba277658758cb2394fe99fb9eb71
Daniel <danil.malaev3@gmail.com>
2026-01-17 21:25:57 +0200

    add new dispatcher

--- src/main/run.py
+++ src/main/run.py
@@ -1,15 +1,15 @@
-from settings.config import TG_BOT_API_KEY
-from handlers import start
 import asyncio
+from settings.config import TG_BOT_API_KEY
 from aiogram import Bot, Dispatcher
 from aiogram.filters import CommandStart
-from aiogram.types import Message
+from handlers import start,random
 
 async def main():
     bot = Bot(token=TG_BOT_API_KEY)
     dispatcher = Dispatcher()
     dispatcher.include_router(start.router)
+    dispatcher.include_router(random.router)
     await dispatcher.start_polling(bot)
 
-if __name__ == "__main__":
-    asyncio.run(main())
+if __name__ == "__main__": asyncio.run(main())
+
