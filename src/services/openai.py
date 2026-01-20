956e4d414903dd7fa6818073b31a21ab2e235593
Daniel <danil.malaev3@gmail.com>
2026-01-20 20:01:57 +0200

    working integration(and fix syntax errors)

--- src/services/openai.py
+++ src/services/openai.py
@@ -1,20 +1,26 @@
-import openai import OpenAi
+from openai import OpenAI
 import logging
-from settings import config.OPENAI_MODEL
+from settings import config
 
-client = new OpenAi(api_key=OPENAI_MODEL)
+logger = logging.getLogger(__name__)
 
-async def generate_text(user_text) -> dict:
+client = OpenAI(api_key=config.AI_API_KEY)
+
+def generate_text(system_text: str,user_text:str) -> str:
 
     try:
 
-        response = await openai.ChatCompletion.acreate(
-            model = OPENAI_MODEL,
+        response = client.chat.completions.create(
+            model = config.OPENAI_MODEL,
             messages = [
+                {"role":"system", "content": system_text},
                 {"role":"user", "content":user_text}
-            ]
+            ],
+            temperature = config.OPENAI_MODEL_TEMPERATURE
         )
         return response.choices[0].message.content
 
-    except Exception as e:
-        logging.error(e)
+    except Exception:
+        logger.exception("OpenAI request failed!")
+        return "OpenAI request failed. Please try again later."
+
