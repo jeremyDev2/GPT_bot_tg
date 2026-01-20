d11674e2affe86a62c4a9c91053c6b95e1d8cb94
Daniel <danil.malaev3@gmail.com>
2026-01-20 10:59:09 +0200

    add path to 'welcome text' from resources and callback function to '/start'

--- src/handlers/start.py
+++ src/handlers/start.py
@@ -2,7 +2,8 @@ from pathlib import Path
 
 from aiogram import Router
 from aiogram.filters import CommandStart
-from aiogram.types import FSInputFile, Message
+from aiogram.fsm.context import FSMContext
+from aiogram.types import CallbackQuery, FSInputFile, Message
 
 from settings import config
 from ui import keyboard
@@ -13,11 +14,31 @@ _START_IMAGE_: Path = config.RESOURCES_DIR / "images" / "main.jpg"
 if not _START_IMAGE_.is_file():
     raise FileNotFoundError(f"Start image not found: {_START_IMAGE_}")
 
+_WELCOME_TEXT_: Path = config.RESOURCES_DIR / "welcome_text" / "main.txt"
+if not _WELCOME_TEXT_.is_file():
+    raise FileNotFoundError(f"Welcome text not found: {_WELCOME_TEXT_}")
+
+
 @router.message(CommandStart())
-async def message_handler(message: Message) -> None:
+async def message_handler(message: Message, state: FSMContext) -> None:
+    await state.clear() # clearing state, because it's "start" FSM 
     photo = FSInputFile(_START_IMAGE_)
+    welcome_text = _WELCOME_TEXT_.read_text(encoding="utf-8")
     await message.answer_photo(
         photo=photo,
-        caption="Starting bot.",
+        caption=welcome_text,
         reply_markup=keyboard.main_keyboard(),
     )
+
+
+@router.callback_query(lambda call: call.data == "start")
+async def start_callback(call: CallbackQuery, state: FSMContext) -> None:
+    await state.clear() # clearing state, because it's "start" FSM 
+    photo = FSInputFile(_START_IMAGE_)
+    welcome_text = _WELCOME_TEXT_.read_text(encoding="utf-8")
+    await call.message.answer_photo(
+        photo=photo,
+        caption=welcome_text,
+        reply_markup=keyboard.main_keyboard(),
+    )
+    await call.answer()
