c7d1d133ce1652e14c8bdce4da08a1e2f93a7947
Daniel <danil.malaev3@gmail.com>
2026-01-23 23:02:25 +0200

    added keyboard

--- src/handlers/talk.py
+++ src/handlers/talk.py
@@ -3,6 +3,7 @@ from aiogram import Router
 from aiogram.types import Message, FSInputFile
 from aiogram.filters import Command
 from settings import config
+from ui.keyboard import celebrity_talk_keyboard
 
 router = Router()
 
@@ -11,6 +12,7 @@ if not _TALK_IMAGE_.is_file():
     raise FileNotFoundError(f"Random image not found: {_TALK_IMAGE_}")
 
 @router.message(Command("talk"))
-async def random_handler(message: Message) -> None:
+async def talk_handler(message: Message) -> None:
     photo = FSInputFile(_TALK_IMAGE_)
-    await message.answer_photo(photo=photo, caption="Talking...")
+    await message.answer_photo(photo=photo, caption="Select famouse person", reply_markup=celebrity_talk_keyboard())
+
