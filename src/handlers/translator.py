31caa5faee3c66fd70d019a79d59eca659f6d940
Daniel <danil.malaev3@gmail.com>
2026-01-25 17:50:25 +0200

    translator implementation

--- src/handlers/translator.py
+++ src/handlers/translator.py
@@ -1,16 +1,39 @@
 from pathlib import Path
-from aiogram import Router
-from aiogram.types import Message, FSInputFile
-from aiogram.filters import Command
+from aiogram import Router, F
+from aiogram.fsm.context import FSMContext
+from aiogram.types import Message, FSInputFile, CallbackQuery
+from aiogram.filters import Command, StateFilter
+from services import openai
+
 from settings import config
+from state.states import TranslatorState
+from ui.keyboard import translator_keyboard
 
 router = Router()
 
 _TRANSLATOR_IMAGE_: Path = config.RESOURCES_DIR / "images" / "translator.jpg"
 if not _TRANSLATOR_IMAGE_.is_file():
-    raise FileNotFoundError(f"Random image not found: {_TRANSLATOR_IMAGE_}")
+    raise FileNotFoundError(f"Translator image not found: {_TRANSLATOR_IMAGE_}")
 
 @router.message(Command("translator"))
-async def random_handler(message: Message) -> None:
+async def translator_handler(message: Message) -> None:
     photo = FSInputFile(_TRANSLATOR_IMAGE_)
-    await message.answer_photo(photo=photo, caption="Translating...")
+    await message.answer_photo(photo=photo, 
+                               caption="Send text to translate", 
+                               reply_markup=translator_keyboard())
+
+@router.callback_query(F.data.startswith("translate_to_"))
+async def callback_query(call:CallbackQuery, state:FSMContext)-> None:
+    lang = call.data.replace("translate_to_", "")
+    await state.update_data(lang=lang)
+    await state.set_state(TranslatorState.user_text)
+    await call.answer()
+
+@router.message(StateFilter(TranslatorState.user_text))
+async def answer_message(message:Message, state:FSMContext):
+    data = await state.get_data()
+    lang = data["lang"]
+    user_text = message.text
+    system_text = "You are a translator"
+    answer = await openai.generate_text(system_text, f"Translate to {lang} : {user_text}")
+    await message.answer(answer)
